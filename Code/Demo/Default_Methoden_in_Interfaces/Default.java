e549f07a584bc77facbcf05e9805aa7fd54f5abd
nobody1337 <98647065+nobody1337@users.noreply.github.com>
2022-06-08 12:01:22 +0200

    Default

--- Code/Demo/Default_Methoden_in_Interfaces/Default.java
+++ Code/Demo/Default_Methoden_in_Interfaces/Default.java
@@ -1,4 +1,10 @@
 package Demo.Default_Methoden_in_Interfaces;
-
+@FunctionalInterface
 public interface Default {
+    //Abstrakte-Methode
+    void bruellen();
+    //Default-Methode
+    default void bruellen_d(){
+        System.out.println("RAAAW,ich bin default");
+    }
 }
