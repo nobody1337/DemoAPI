76cd4a0b9936fd584bd94b1182a46b1078f83da2
nobody1337 <98647065+nobody1337@users.noreply.github.com>
2022-06-08 12:11:17 +0200

    Parametercheck

--- Code/Demo/Parameter_Check/Parametercheck.java
+++ Code/Demo/Parameter_Check/Parametercheck.java
@@ -1,4 +1,23 @@
 package Demo.Parameter_Check;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Parametercheck {
+
+    static List<String>liste= new ArrayList<>();
+    public static void main(String[] args) {
+
+        String wort = "Hallo";
+        String wort2 = null;
+        add(wort);
+        add(wort2);
+
+    }
+    public static void add(String wort){
+        Objects.requireNonNull(wort);
+        liste.add(wort);
+    }
 }
