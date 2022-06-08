98d52424ea50c23a5591ebb5922b563430e3424a
nobody1337 <98647065+nobody1337@users.noreply.github.com>
2022-06-08 11:49:17 +0200

    Overloading

--- Code/Demo/Vermeide_Overloading_Methoden/Overlaoding.java
+++ Code/Demo/Vermeide_Overloading_Methoden/Overlaoding.java
@@ -1,4 +1,17 @@
 package Demo.Vermeide_Overloading_Methoden;
 
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public interface Overlaoding {
+
+    void addRenderer(Function<Overlaoding,String> renderer);
+    void addLogCondition(Predicate<Overlaoding> logCondition);
+
+    void add(Function<Overlaoding, String> renderer);
+    void add(Predicate<Overlaoding> logCondition);
+
+//    void add(int x,int y);
+//    void add(int x,int z);
 }
